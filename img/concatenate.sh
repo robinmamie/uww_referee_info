9d92e9b72fbbf89c782412239ee9def9f63a7a94
Robin Mamié <robin@mamie.one>
2023-08-08 21:38:23 +0200

    Fix concatenate.sh for systems without magick

--- img/concatenate.sh
+++ img/concatenate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-magick montage stats_RCM.png stats_IS.png stats_I.png stats_II.png stats_III.png -mode Concatenate -tile 1x stats.png
+montage stats_RCM.png stats_IS.png stats_I.png stats_II.png stats_III.png -mode Concatenate -tile 1x stats.png
