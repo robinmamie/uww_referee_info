12a258221d526a2d7fc4e6ee83e2839cf6daf093
Robin Mamié <robin@mamie.one>
2024-09-23 14:18:37 +0200

    Factorize code for maintenance

--- update_referee_cards.py
+++ update_referee_cards.py
@@ -26,36 +26,10 @@ def get_emoji_flag(country2: str):
     return a + b
 
 
-def create_referee(data):
+def update_card(data):
     new_card = card.replace("<!--current_day-->", date)
     new_card = new_card.replace("<!--is_active-->", data['is_active'])
     new_card = new_card.replace("<!--name-->", data['name'])
-    category = data['category'] if data['category'] in ["III","II","I","IS","RCM"] else "?"
-    new_card = new_card.replace("<!--category-->", category)
-    new_card = new_card.replace("<!--photo-->", data['photo'])
-    new_card = new_card.replace("<!--birthdate-->", data['birthdate'])
-    new_card = new_card.replace("<!--country-->", data['country'])
-    sex = "♀" if data['sex'] == "F" else "♂" if data['sex'] == "M" else "?"
-    new_card = new_card.replace("<!--sex-->", sex)
-    country2 = cc.convert(names = data['country'][:3], src='IOC', to='ISO2')
-    emoji_flag = get_emoji_flag(country2)
-    new_card = new_card.replace("<!--emoji_flag-->", emoji_flag)
-    new_page = page.replace("<!--new_info-->", new_card)
-    new_page = new_page.replace("<!--id_number-->", data['id_number'])
-    with open(f"{base_path}/{int(data['id_number']):07d}.html", "w") as f:
-        f.write(new_page)
-
-
-def change_referee(current, changes):
-    filename = [filename for filename in os.listdir(f'{base_path}') if changes['key'] in filename][0]
-    with open(f"{base_path}/{filename}") as f:
-        page = f.read()
-    page = page.replace(history_header, "")
-
-    data = current[changes['key']]
-    
-    new_card = card.replace("<!--current_day-->", date)
-    new_card = new_card.replace("<!--is_active-->", data['is_active'])
     category = data['category'] if data['category'] in ["III","II","I","IS","RCM"] else "?"
     new_card = new_card.replace("<!--category-->", category)
     new_card = new_card.replace("<!--photo-->", data['photo'])
@@ -67,7 +41,32 @@ def change_referee(current, changes):
     emoji_flag = get_emoji_flag(country2)
     new_card = new_card.replace("<!--emoji_flag-->", emoji_flag)
     new_card = new_card.replace("<!--id_number-->", data['id_number'])
-    new_card = new_card.replace("<!--name-->", data['name'])
+    return new_card
+
+
+def update_page_on_disk(new_card, id_number):
+    html_filename = "f{int(id_number):07d}.html"
+    filename = [filename for filename in os.listdir(f'{base_path}') if html_filename in filename][0]
+    with open(f"{base_path}/{filename}") as f:
+        page = f.read()
+    page = page.replace(history_header, "")
+    new_page = page.replace("<!--new_info-->\n", new_card)
+    with open(f"{base_path}/{html_filename}", "w") as f:
+        f.write(new_page)
+
+
+def create_referee(data):
+    new_card = update_card(data)
+    new_page = page.replace("<!--new_info-->", new_card)
+    new_page = new_page.replace("<!--id_number-->", data['id_number'])
+    with open(f"{base_path}/{int(data['id_number']):07d}.html", "w") as f:
+        f.write(new_page)
+
+
+def change_referee(current, changes):
+    data = current[changes['key']]
+    new_card = update_card(data)
+    
     data_changed = list(changes['changes'].keys())
     if "category" in data_changed:
         new_card = new_card.replace("blink_me__category", "blink_me__")
@@ -79,35 +78,13 @@ def change_referee(current, changes):
         new_card = new_card.replace("blink_me__country", "blink_me__")
     if "sex" in data_changed or "birthdate" in data_changed:
         new_card = new_card.replace("blink_me__other", "blink_me__")
-    new_page = page.replace("<!--new_info-->\n", new_card)
-    with open(f"{base_path}/{int(data['id_number']):07d}.html", "w") as f:
-        f.write(new_page)
+
+    update_page_on_disk(new_card, changes["key"])
 
 
 def remove_referee(data):
-    filename = [filename for filename in os.listdir('{base_path}') if data['id_number'] in filename][0]
-    with open(f"{base_path}/{filename}") as f:
-        page = f.read()
-    page = page.replace(history_header, "")
-    
-    new_card = card.replace("<!--current_day-->", date)
-    new_card = new_card.replace("<!--is_active-->", "null")
-    category = data['category'] if data['category'] in ["III","II","I","IS","RCM"] else "?"
-    new_card = new_card.replace("<!--category-->", category)
-    new_card = new_card.replace("<!--photo-->", data['photo'])
-    new_card = new_card.replace("<!--birthdate-->", data['birthdate'])
-    new_card = new_card.replace("<!--country-->", data['country'])
-    sex = "♀" if data['sex'] == "F" else "♂" if data['sex'] == "M" else "?"
-    new_card = new_card.replace("<!--sex-->", sex)
-    country2 = cc.convert(names = data['country'][:3], src='IOC', to='ISO2')
-    emoji_flag = get_emoji_flag(country2)
-    new_card = new_card.replace("<!--emoji_flag-->", emoji_flag)
-    new_card = new_card.replace("<!--id_number-->", data['id_number'])
-    new_card = new_card.replace("<!--name-->", data['name'])
-
-    new_page = page.replace("<!--new_info-->\n", new_card)
-    with open(f"{base_path}/{int(data['id_number']):07d}.html", "w") as f:
-        f.write(new_page)
+    new_card = update_card(data)
+    update_page_on_disk(new_card, data["id_number"])
 
 
 def main():
@@ -159,8 +136,14 @@ def main():
             for col, from_to in changes['changes'].items():
                 if col == 'photo':
                     print(f"<li>The profile picture</li>")
+                elif col == 'is_active':
+                    if from_to[1]:
+                        print("<li>Licence is now active.</li>")
+                    else:
+                        print("<li>Licence has been deactivated.</li>")
                 else:
-                    print(f"<li>{col.title().replace('_', ' ')} from {from_to[0]} to {from_to[1]}</li>")
+                    fromStr = from_to[0] if from_to[0] else "?"
+                    print(f"<li>{col.title().replace('_', ' ')} from {fromStr} to {from_to[1]}</li>")
             print("</ul>")
             print("</li>")
         print("</ul>")
