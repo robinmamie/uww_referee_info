75fee183b7527c9da722e4ed5e6dc0204640f661
Robin Mamié <robin@mamie.one>
2024-09-27 18:32:04 +0200

    Update with dom, better CSS

--- update_referee_cards.py
+++ update_referee_cards.py
@@ -1,28 +1,30 @@
 import country_converter as coco
 
-import datetime
+import glob
 import os
 import sys
 
+from bs4 import BeautifulSoup
 from csv_diff import load_csv, compare
+from datetime import datetime
 from tqdm import tqdm
 
 
 cc = coco.CountryConverter()
 emoji_a = 127462
 base_path = "referees"
-history_header = '<button id="toggle">Show History</button>\n<div id="history">\n'
-date = datetime.datetime.today().strftime('%Y-%m-%d')
-blink_class_prefix = "blink_me__"
+date = datetime.today().strftime('%Y-%m-%d')
+possible_categories = ["III","II","I","IS","RCM"]
+BLINK_CLASS = "blink_off"
 
-with open("uww_card.html") as f:
-    card = f.read()
-with open("uww_referee.html") as f:
-    page = f.read()
+def format_date(date_string: str) -> str:
+    new_date = datetime.strptime(date_string, "%Y-%m-%d")
+    return new_date.strftime("%-d %b %Y")
 
 
 def get_emoji_letter_flag(letter: str) -> str:
-    return "&#x" + format(emoji_a + ord(letter) - ord("A"), "X")
+    hexValue = format(emoji_a + ord(letter) - ord("A"), "X")
+    return chr(int(hexValue, 16))
 
 
 def get_emoji_flag(country2: str) -> str:
@@ -33,63 +35,111 @@ def get_ref_path(id_number: str) -> str:
     return f"{base_path}/{int(id_number):07d}.html"
 
 
-def update_card(data: dict) -> str:
-    new_card = card.replace("<!--current_day-->", date)
-    new_card = new_card.replace("<!--is_active-->", data['is_active'])
-    new_card = new_card.replace("<!--name-->", data['name'])
-    category = data['category'] if data['category'] in ["III","II","I","IS","RCM"] else "?"
-    new_card = new_card.replace("<!--category-->", category)
-    new_card = new_card.replace("<!--photo-->", data['photo'])
-    new_card = new_card.replace("<!--birthdate-->", data['birthdate'])
-    new_card = new_card.replace("<!--country-->", data['country'])
-    sex = "♀" if data['sex'] == "F" else "♂" if data['sex'] == "M" else "?"
-    new_card = new_card.replace("<!--sex-->", sex)
+def get_change_reason(data: dict, changes: dict) -> str:
+    if "category" in changes:
+        change = changes['category']
+        if change[1] not in possible_categories:
+            return "Category erased on"
+        if change[0] not in possible_categories:
+            return "Category readded on"
+        if possible_categories.index(change[0]) <= possible_categories.index(change[1]):
+            return "Category upgraded on"
+        else:
+            return "Category downgraded on"
+    if "name" in changes:
+        return "Named changed on"
+    if "sex" in changes:
+        return "Gender changed on"
+    if "country" in changes:
+        return "Country changed on"
+    if "birthdate" in changes:
+        return "Birthdate changed on"
+    if "photo" in changes:
+        return "Photo changed on"
+    if "new_is_active" in changes:
+        return "Activity checked on"
+    if "is_active" in changes:
+        return "Active as of" if data['is_active'] else "Inactive as of"
+    return "Effective from"
+
+
+def setValues(doc: BeautifulSoup, id: str, newValues: list[str], attributes: list[str]) -> None:
+    node = doc.find(id=id)
+    for newValue, attribute in zip(newValues, attributes):
+        if attribute == 'text':
+            node.string = newValue
+        else:
+            node[attribute] = newValue
+    node['id'] = f"{id}_{date}"
+
+
+def setValue(doc: BeautifulSoup, id: str, newValue: str, attribute='text') -> None:
+    setValues(doc, id, [newValue], [attribute])
+
+
+def update_card(data: dict, reason: str, is_removed=False) -> BeautifulSoup:
+    with open("uww_card.html") as f:
+        card = BeautifulSoup(f.read(), 'html.parser')
+
+    referee_id = data['id_number']
     country2 = cc.convert(names = data['country'][:3], src='IOC', to='ISO2')
     emoji_flag = get_emoji_flag(country2)
-    new_card = new_card.replace("<!--emoji_flag-->", emoji_flag)
-    new_card = new_card.replace("<!--id_number-->", data['id_number'])
-    return new_card
+    sex = "♀" if data['sex'] == "F" else "♂" if data['sex'] == "M" else "♂♀"
+    category = data['category'] if data['category'] in possible_categories else "?"
+
+    setValues(card, 'state', [], [])
+    setValue(card, 'stateTitle', f"{reason} {format_date(date)}")
+    setValue(card, 'userAthena', data['athena'], 'href')
+    setValues(card, 'userPhoto', [data['photo'], f"Photo of referee {data['name']}"], ['src', 'alt'])
+    if 'is_active' in data or is_removed:
+        activity = data['is_active'] if not is_removed else 'over'
+    else:
+        activity = 'unknown'
+    setValue(card, 'right', f"insideColumn right activity{activity.title()}", 'class')
+    setValue(card, 'userName', data['name'])
+    setValue(card, 'userCountry', f"{data['country']} {emoji_flag}")
+    setValues(card, 'userBirthdate', [format_date(data['birthdate']), data['birthdate']], ['text', 'iso'])
+    setValue(card, 'userSex', sex)
+    setValue(card, 'userCategory', f"Category {category}")
+    
+    return card
 
 
-def update_page_on_disk(new_card: str, id_number: str) -> None:
-    html_filename_path = get_ref_path(id_number)
-    with open(html_filename_path) as f:
-        new_page = f.read()
-    new_page = new_page.replace(history_header, "")
-    new_page = new_page.replace("<!--new_info-->\n", new_card)
-    with open(html_filename_path, "w") as f:
-        f.write(new_page)
-
-
-def create_referee(data: dict, already_exists: bool) -> None:
-    new_card = update_card(data)
-    new_page = page
+def update_referee(card: BeautifulSoup, already_exists: bool, id_number: str) -> None:
+    path = "uww_referee.html"
     if already_exists:
-        html_filename_path = get_ref_path(data['id_number'])
-        with open(html_filename_path) as f:
-            new_page = f.read()
-    new_page = page.replace("<!--new_info-->", new_card)
-    new_page = new_page.replace("<!--id_number-->", data['id_number'])
-    with open(get_ref_path(data['id_number']), "w") as f:
-        f.write(new_page)
+        path = get_ref_path(id_number)
+    with open(path) as f:
+        page = BeautifulSoup(f.read(), 'html.parser')
+
+    page.find(id="pageTitle").string = f"Referee #{id_number}"
+    currentState = page.find(id="currentState").find("div")
+    if currentState:
+        page.find(id="historyContent").insert_after(currentState)
+    page.find(id="currentState").append(card.extract())
+
+    with open(get_ref_path(id_number), "w") as f:
+        f.write(str(page))
+
+
+def create_referee(data: dict, already_exists: bool, reason="Created on") -> None:
+    update_referee(update_card(data, reason), False, data['id_number'])
 
 
 def change_referee(current: dict, changes: dict) -> None:
     data = current[changes['key']]
-    new_card = update_card(data)
+    card = update_card(data, get_change_reason(data, changes['changes']))
     
     for item_changed in list(changes['changes'].keys()):
-        if item_changed in ["sex", "birthdate"]:
-            new_card = new_card.replace(f"{blink_class_prefix}other", blink_class_prefix)
-        else:
-            new_card = new_card.replace(f"{blink_class_prefix}{item_changed}", blink_class_prefix)
+        if not "is_active" in item_changed:
+            elementChanged = card.find(id=f"user{item_changed.title()}_{date}")
+            elementChanged['class'] = [BLINK_CLASS, item_changed]
 
-    update_page_on_disk(new_card, changes["key"])
+    update_referee(card, True, changes["key"])
 
 
 def remove_referee(data: dict) -> None:
-    new_card = update_card(data)
-    update_page_on_disk(new_card, data["id_number"])
+    update_referee(update_card(data, "Retired as of", True), True, data["id_number"])
 
 
 def main() -> None:
@@ -97,13 +147,17 @@ def main() -> None:
     current = load_csv(open("uww_referees.csv"), key="id_number")
     diff = compare(old, current)
     
-    for changes in tqdm(diff['changed']):
-        change_referee(current, changes)
-    for new_ref in tqdm(diff['added']):
+    for changes in diff['changed']:
+        if changes['changes'] != 1 and not 'athena' in changes['changes']:
+            change_referee(current, changes)
+    for new_ref in diff['added']:
         already_exists = os.path.isfile(get_ref_path(new_ref['id_number']))
         create_referee(new_ref, already_exists)
-    for old_ref in tqdm(diff['removed']):
+    for old_ref in diff['removed']:
         remove_referee(old_ref)
+    if diff['columns_added']:
+        for key in current.keys():
+            change_referee(current, {'key': key, 'changes': {'new_is_active': ''}})
 
     sys.stdout = open('referee_changes.html','wt')
     print("""<!doctype html>
