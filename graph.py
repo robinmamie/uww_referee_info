9d479320950032b073556b30799f84ca522eefb4
Robin Mamié <robin@mamie.one>
2026-01-02 14:35:03 +0100

    Update graph composition

--- graph.py
+++ graph.py
@@ -5,14 +5,9 @@ import pandas as pd
 import sys
 
 
-def get_ref_count(df, category, sex):
-    df_tmp = df[((df['category'] == category) & (df['sex'] == sex))].groupby(df["birthdate"].dt.year).count()
-    return df_tmp.reindex(range(min(df.birthdate).year, max(df.birthdate).year+1), fill_value=0)
-
-
-def get_inactive_referees(df, category):
-    df_tmp = df[((df['category'] == category) & (~df['is_active']))].groupby(df["birthdate"].dt.year).count()
-    return df_tmp.reindex(range(min(df.birthdate).year, max(df.birthdate).year+1), fill_value=0)
+def get_ref_count(df_orig, df, sex):
+    df_tmp = df[(df['sex'] == sex)].groupby(df["birthdate"].dt.year).count()
+    return df_tmp.reindex(range(min(df_orig.birthdate).year, max(df_orig.birthdate).year+1), fill_value=0)
 
 
 def graph_data():
@@ -24,13 +19,14 @@ def graph_data():
     }
     df_uww_all = pd.read_csv('uww_referees.csv')
     df_uww_all['birthdate'] = pd.to_datetime(df_uww_all['birthdate'])
-    df_uww_all = df_uww_all.loc[df_uww_all['category'].isin(colors)]
+    df_uww_all = df_uww_all.loc[df_uww_all['category'].isin(list(colors.keys()) + ['IS-RCM'])]
 
     for category in colors:
-        df_tmp_m = get_ref_count(df_uww_all, category, 'M')
-        df_tmp_f = get_ref_count(df_uww_all, category, 'F')
+        df_cat = df_uww_all[((df_uww_all['category'] == category) | (df_uww_all['category'] == f"{category}-RCM"))]
+        df_tmp_m = get_ref_count(df_uww_all, df_cat, 'M')
+        df_tmp_f = get_ref_count(df_uww_all, df_cat, 'F')
 
-        nb_ref_cat_all =  df_uww_all[df_uww_all['category'] == category]['name'].count()
+        nb_ref_cat_all =  df_cat['name'].count()
 
         df_tmp = pd.DataFrame({
             f"Female ({df_tmp_f['name'].sum()})": df_tmp_f['name'],
@@ -44,6 +40,7 @@ def graph_data():
         ax.set_ylim(0, 40)
         ax.grid(axis='y')
         plt.savefig(f'img/stats_{category}.png', dpi=300, bbox_inches='tight')
+        plt.close(ax.get_figure())
 
 
 def main():
